79823cedf318f1562ce887d6bfec01f9b140de91
Aarav Raj <aaravrajtron@gmail.com>
2025-03-04 20:47:28 +0530

    scripting

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function createChatBox(html,classes) {
 }
 
 
-
+// function to handle the user chat after enter in user chat box 
 function handlechatResponse(message) {
   let html=`<img src="images/pngwing.com.png" alt="" id="userImage" width="36">
         <div class="user-chat-area">
@@ -16,7 +16,8 @@ function handlechatResponse(message) {
         let userChatBox=createChatBox(html,"user-chat-box")
 }
 
-
+// if event key is enter then execute it 
+// after enter handles the data written in user text box
 prompt.addEventListener("keydown",(e)=>{
   if(e.key=="Enter") {
     handlechatResponse(prompt.value)
